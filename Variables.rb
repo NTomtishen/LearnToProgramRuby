422c4e43e74d3138eb44093fec0938292cece5eb
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-07-20 13:45:26 -0400

    Variables.rb, a program with examples from Chapter 4.

---
+++ Variables.rb
@@ -0,0 +1,44 @@
+puts '...you can say that again...'
+puts '...you can say that again...'
+
+puts ''
+
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+
+puts ''
+
+name = 'Meeko Tomtishen'
+puts 'My dog\'s name is ' + name + '.'
+
+puts ''
+
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally'
+
+puts ''
+
+my_own_var = 'just another' + ' string'
+puts my_own_var
+
+my_own_var = 5 * (1 + 2)
+puts my_own_var
+
+puts ''
+
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+
+puts ''
+
+var1 = 'eight'
+puts var1
+puts var2
+
+puts ''
