6be0edeec593e183047d0803dfd03a32043557d5
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-07-20 13:26:33 -0400

    Adds 2ChallengeProbs.rb where I answer the Challenge Problems from Chapter 2.

---
+++ 2ChallengeProbs.rb
@@ -0,0 +1,8 @@
+# How many hours are in a year?
+puts 24 * 365
+# How many minutes are in a decade?
+puts 60 * 24 * 365 * 10
+# How many seconds old are you?
+puts 60 * 60 * 24 * 365 * 18
+# If I am 912 million seconds old, how old am I?
+puts 912000000.0/ (60 * 60 * 24 * 365)
