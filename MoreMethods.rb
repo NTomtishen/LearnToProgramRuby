171f30583089f008377d579fadffb6aef0769506
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-07-21 09:47:58 -0400

    MoreMethods.rb, a program with examples from Chapter 6.

---
+++ MoreMethods.rb
@@ -0,0 +1,28 @@
+# Some string methods
+
+var1 = 'stop'
+var2 = 'deliver repaid desserts'
+var3 = 'Can you pronounce this sentence backward?'
+
+puts var1.reverse
+puts var2.reverse
+puts var3.reverse
+puts var1
+puts var2
+puts var3
+
+puts ''
+
+
+# puts 'What is your full name?'
+# name = gets.chomp
+# puts 'Did you know there are ' + name.length.to_s + ' characters'
+# puts 'in your name, ' + name + '?'
+
+letters = 'aAbBcBdDeE'
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
