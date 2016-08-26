99ada9392a0655fe9cae233b2d697ede02563004
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-08-26 09:16:01 -0400

    Practice with various array methods

---
+++ ArrayMethods.rb
@@ -0,0 +1,17 @@
+foods = ['artichoke', 'brioche', 'caramel']
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join( ', ' )
+puts
+puts foods.join(' :) ') + ' 8)'
+200.times do puts []
+end
+
+drinks = ['coke', 'water', 'pepsi']
+silverware = ['fork', 'knife', 'spoon']
+
+mealitems = [foods, drinks, silverware]
+
+puts mealitems
