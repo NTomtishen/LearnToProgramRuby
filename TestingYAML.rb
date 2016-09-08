f3ac4d300875afe7f1e253a425f8cb8cb1a37ad8
NTomtishen <ntomtishen@shortridgeacademy.com>
2016-09-08 09:32:29 -0400

    More testing of YAML

--- TestingYAML.rb
+++ TestingYAML.rb
@@ -2,7 +2,16 @@ require 'yaml'
 
 test_array = ['Don\'t give Quiche a chance',
               'Mutants are IN!',
-              'Life-Forms? No Thanks']
+              'Life-Forms? No Thanks',
+            ['Arrays within Arrays',
+              'Crazy Stuff',
+              ['Arrays Within Arrays within Arrays',
+                'Arrayception']],
+                'Now back to your original Array',
+              '42',
+              42,
+              'true',
+              false]
 
 test_string = test_array.to_yaml
 
@@ -11,10 +20,3 @@ filename = 'Examples.txt'
 File.open filename, 'w' do |f|
   f.write test_string
 end
-
-read_string = File.read filename
-
-read_array = YAML::load read_string
-
-puts(read_string = test_string)
-puts(read_array = test_array)
